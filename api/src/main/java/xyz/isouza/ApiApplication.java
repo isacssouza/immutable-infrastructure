cef746312ac7055ca77f0f3a4ae342d620fde450
Isac Souza <isouza@daitangroup.com>
2017-09-17 10:01:58 -0300

    Initial commit

---
+++ api/src/main/java/xyz/isouza/ApiApplication.java
@@ -0,0 +1,14 @@
+package xyz.isouza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+}
