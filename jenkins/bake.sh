d0e358e4df0c0c36ce6969f3774a6ebb0491819c
Isac Souza <isouza@daitangroup.com>
2017-09-21 00:11:28 -0300

    Fix api service

--- jenkins/bake.sh
+++ jenkins/bake.sh
@@ -3,7 +3,7 @@
 set -ex
 
 cd ${WORKSPACE}/packer
-packer build -only amazon-ebs -var jar_path=api-0.0.1-SNAPSHOT.jar api.json \
-    | grep 'amazon-ebs: AMI: ami-' \
+packer build -only amazon-ebs -var jar_path=api-0.0.1-SNAPSHOT.jar api.json | tee packer-out.txt
+grep 'amazon-ebs: AMI: ami-' packer-out.txt \
     | grep -o 'ami-.*$' \
     > ami-id.txt
