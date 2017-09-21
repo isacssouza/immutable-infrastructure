28fb61e86ef34ef07163494b485b6b96d69418f8
Isac Souza <isouza@daitangroup.com>
2017-09-21 00:33:22 -0300

    Fix regex

--- jenkins/bake.sh
+++ jenkins/bake.sh
@@ -5,5 +5,5 @@ set -ex
 cd ${WORKSPACE}/packer
 packer build -only amazon-ebs -var jar_path=api-0.0.1-SNAPSHOT.jar api.json | tee packer-out.txt
 grep 'amazon-ebs: AMI: ami-' packer-out.txt \
-    | grep -o 'ami-[0-9a-z]+' \
+    | grep -o 'ami-[0-9a-z]\+' \
     > ami-id.txt
