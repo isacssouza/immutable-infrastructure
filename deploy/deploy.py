c8164b38d7ce8491804e6ecbf950521580e3cc41
Isac Souza <isouza@daitangroup.com>
2017-09-22 10:47:33 -0300

    remove delete of new stack

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -90,7 +90,7 @@ def main():
         delete_old(version)
     else:
         print('New version is not healthy, deleting new version.')
-        delete_new(version)
+        # delete_new(version)
 
 
 if __name__ == '__main__':
