1523e76f84f3eb947d34844446cf6748f8d05617
Isac Souza <isouza@daitangroup.com>
2017-09-21 22:58:44 -0300

    add deploy.py script

---
+++ deploy/deploy.py
@@ -0,0 +1,91 @@
+from argparse import ArgumentParser
+import subprocess
+import boto3
+import urllib2
+
+
+def parse_args():
+    parser = ArgumentParser(description='Deploy the application.')
+    parser.add_argument('version', type=str, help='The version number for this deployment.')
+    parser.add_argument('ami_id', type=str, help='Api AMI id to deploy.')
+    return parser.parse_args()
+
+
+def deploy(version, ami_id):
+    print("Deploying version {} using AMI {}.".format(version, ami_id))
+
+    command = "ansible-playbook "
+    command += "-e \"api_ami_id={}\" ".format(ami_id)
+    command += "-e \"version={}\" ".format(version)
+    command += "-t vpc,security-group,sns-topic,api-elb,api "
+    command += "playbooks/deploy.yml"
+
+    subprocess.call(command, shell=True)
+
+
+def is_app(tags):
+    return tags.get('Application') == 'true'
+
+
+def is_version(tags, version):
+    return tags.get('Version') == version
+
+
+def to_tag_dict(tags):
+    return {tag['Key']:  tag['Value'] for tag in tags}
+
+
+def delete_old(new_version):
+    client = boto3.client('cloudformation')
+
+    all_stacks = client.describe_stacks()
+
+    for stack in all_stacks['Stacks']:
+        tags = to_tag_dict(stack['Tags'])
+        if is_app(tags) and not is_version(tags, new_version):
+            print('Deleting version {}.'.format(tags['Version']))
+            client.delete_stack(StackName=stack['StackName'])
+
+
+def delete_new(version):
+    client = boto3.client('cloudformation')
+
+    client.delete_stack(StackName='api-{}'.format(version))
+
+
+def is_service_up(dns):
+    return urllib2.urlopen('http://{}/'.format(dns)).getcode() == 200
+
+
+def is_version_healthy(version):
+    client = boto3.client('cloudformation')
+    stacks = client.describe_stacks(StackName='api-{}'.format(version))
+    stack = stacks['Stacks'][0]
+
+    for output in stack['Outputs']:
+        if output['OutputKey'] == 'ElbDns':
+            dns = output['OutputValue']
+            return is_service_up(dns)
+
+    return False
+
+
+def main():
+    args = parse_args()
+    version = args.version
+    ami_id = args.ami_id
+
+    deploy(version, ami_id)
+
+    print('Version {} deployed.'.format(version))
+
+    if is_version_healthy(version):
+        print('New version is healthy, deleting old versions.')
+        delete_old(version)
+    else:
+        print('New version is not healthy, deleting new version.')
+        delete_new(version)
+
+
+if __name__ == '__main__':
+    main()
