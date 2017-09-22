fbc156f54bb771a02ea7b1c29788deeab43c2af7
Isac Souza <isouza@daitangroup.com>
2017-09-22 01:23:45 -0300

    Revert "Boom!"
    This reverts commit 89ea33a923692f6660b66d63f98d8760414c2b1e.

--- api/src/main/java/xyz/isouza/controller/HelloController.java
+++ api/src/main/java/xyz/isouza/controller/HelloController.java
@@ -1,13 +1,12 @@
 package xyz.isouza.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/hello")
 public class HelloController {
 
-    @PostMapping
+    @GetMapping
     public String sayHello() {
         return "Hello, world!";
     }
