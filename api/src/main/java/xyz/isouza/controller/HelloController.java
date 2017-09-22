89ea33a923692f6660b66d63f98d8760414c2b1e
Isac Souza <isouza@daitangroup.com>
2017-09-22 00:46:53 -0300

    Boom!

--- api/src/main/java/xyz/isouza/controller/HelloController.java
+++ api/src/main/java/xyz/isouza/controller/HelloController.java
@@ -1,12 +1,13 @@
 package xyz.isouza.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/hello")
 public class HelloController {
 
-    @GetMapping
+    @PostMapping
     public String sayHello() {
         return "Hello, world!";
     }
