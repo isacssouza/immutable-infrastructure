d9ba4b19cc3875bb6c46e566ae00182b6332b158
Isac Souza <isouza@daitangroup.com>
2017-12-05 22:17:57 -0200

    Use us-east-2 when starting jenkins

--- jenkins/start-jenkins.sh
+++ jenkins/start-jenkins.sh
@@ -5,7 +5,7 @@ aws_profile=${2:-default}
 
 export AWS_PROFILE=${aws_profile}
 docker run \
-    -e AWS_REGION=$(aws configure get region) \
+    -e AWS_REGION=us-east-2 \
     -e AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id) \
     -e AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key) \
     -v /var/run/docker.sock:/var/run/docker.sock \
